41d88ef03ce20aa539931ed9bc95eeeacad08ee0
jsand <jsandicr16@gmail.com>
2022-11-06 15:48:23 -0600

    Se agregaron permisos de cors

--- app.js
+++ app.js
@@ -14,6 +14,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
+  next();
+});
+
 let driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('', ''));
 let session = driver.session();
 
@@ -43,7 +57,7 @@ app.get('/buscar/:termino', (req, res) => {
     });
 })
 
-app.get('/:id', (req, res) => {
+app.get('/canciones/:id', (req, res) => {
   session
     //Encuentra el nodo Song con el id enviado y retorna las relaciones de tipo Genre y Singer
     .run('MATCH(C:song)-[:SINGER]->(S:singer),(N)-[:GENRE]->(G:genre) WHERE ID(N) = '+req.params.id.toString()+' RETURN ID(C), C, S, G')
